730981fbb757665cda9aaa9d288d0547125cbb65
wangchao <wangchao@golaxy.cn>
2023-12-08 17:05:49 +0800

    feat:使用vite-plugin-pages实现路由自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Pages({
+      // dirs: ['src/views'],
+      exclude: ['**/components/*.vue']
+    })
   ],
   resolve: {
     alias: {
